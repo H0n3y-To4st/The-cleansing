3ccfdc265fff2c66c480aace378ebfc187dc464e
Abdul Alian <abdulalian@gmail.com>
2021-11-07 20:47:14 +0000

    test

--- Assets/GameModules/GameController.cs
+++ Assets/GameModules/GameController.cs
@@ -8,28 +8,37 @@ public class GameController : MonoBehaviour
 {
 
     [SerializeField] private GameObject Player = null; 
+    private int Level = 1;
+    private int Exp;
+    private int MaxExp = 10;
     [SerializeField] private GameObject Zombie = null; 
-
     [SerializeField] private int PlayerHealth = 0; 
+
     [SerializeField] private int MaxPlayerHealth = 0; 
     [SerializeField] private int ZombieHealth = 0; 
     [SerializeField] private int MaxZombieHealth = 0; 
     [SerializeField] private Button AttackButton = null;
     [SerializeField] private Button BagButton = null;
-
-
-    private bool isItPlayerTurn = false;
-    private string[] RandomItemDroplist = {"Pistol","AK47"};
+    [SerializeField] private GameObject RetryPanel; 
+    [SerializeField] private GameObject NextLevelPanel;
+    [SerializeField] private GameObject BattlePanel;
+    [SerializeField] private GameObject BagPanel;
     [SerializeField] private Text PlayerHP = null; 
     [SerializeField] private Text ZombieHP = null;
+    private bool isItPlayerTurn = false;
+    private string[] RandomItemDroplist = {"Pistol","AK47"};
 
-    void newRound(){
+    public void newRound(){
+        RetryPanel.SetActive(false);
+        NextLevelPanel.SetActive(false);
+        AttackButton.interactable = true; 
+        BagButton.interactable = true;
         PlayerHealth = 100; 
         MaxPlayerHealth = 100; 
         ZombieHealth = 100;
         MaxZombieHealth = 100; 
         isItPlayerTurn = true;
-        UpdateHealthBar(Player, "Player Health:"+PlayerHealth+"/"+MaxPlayerHealth);
+        UpdateHealthBar(Player, "Player Health:"+PlayerHealth+"/"+MaxPlayerHealth+"\nLevel:"+Level+"\nExp:"+Exp+"/"+MaxExp);
         UpdateHealthBar(Zombie, "Zombie Health:"+ZombieHealth+"/"+MaxZombieHealth);
     }
 
@@ -44,15 +53,23 @@ public class GameController : MonoBehaviour
             UpdateHealthBar(Zombie, "Zombie Health:"+ZombieHealth+"/"+MaxZombieHealth);
         }else{
             PlayerHealth  -= damage;    
-            UpdateHealthBar(Player, "Player Health:"+PlayerHealth+"/"+MaxPlayerHealth);
+             UpdateHealthBar(Player, "Player Health:"+PlayerHealth+"/"+MaxPlayerHealth+"\nLevel:"+Level+"\nExp:"+Exp+"/"+MaxExp);
         }
         ChangeTurn();
     }
 
-    private void showNotification(){
-        Debug.Log("User has lost HP.");
+    private void showNotification(string text){
+
     }
 
+    private void AddExp(int exp){
+        Exp += exp; 
+        if(Exp >= MaxExp){
+            Exp = 0; 
+            Level += 1;
+            MaxExp *= 3;
+        }
+    }
     private void UpdateHealthBar(GameObject target, string newTxt){
         if(target == Zombie){
             ZombieHP.text = newTxt;
@@ -61,12 +78,27 @@ public class GameController : MonoBehaviour
         }
     }
     public void BtnAttack(){
-        Attack(Zombie,10);
-        
+        AttackButton.interactable = false; 
+        BattlePanel.SetActive(true);
     }
 
+    public void UsePistol(){
+        BattlePanel.SetActive(false);
+        Attack(Zombie,40);
+    }
+    public void useAk(){
+        BattlePanel.SetActive(false);
+        Attack(Zombie,60);
+    }
     private void ChangeTurn(){
         isItPlayerTurn  = !isItPlayerTurn;
+        if(PlayerHealth <= 0){
+             Die(Player);
+             return;
+        }else if(ZombieHealth <= 0){
+            Die(Zombie);
+            return;
+        }
         if(!isItPlayerTurn){
             StartCoroutine(EnemyTurn());
             AttackButton.interactable = false;
@@ -79,7 +111,30 @@ public class GameController : MonoBehaviour
 
     private IEnumerator EnemyTurn(){
         yield return new WaitForSeconds(2); 
-        Attack(Player,10);
+        Attack(Player,Random.Range(1,60));
+    }
+    private void Die(GameObject target){
+        AttackButton.interactable = false;
+         BagButton.interactable = false;
+            if(target == Player){
+                RetryPanel.SetActive(true);
+            }else if(target == Zombie){
+               NextLevelPanel.SetActive(true);
+                AddExp(5);
+            }
+    }
+
+    public void DrinkWater(){
+        if(PlayerHealth >= 75) return; 
+        PlayerHealth += 25;
+        BagPanel.SetActive(false);
+        ChangeTurn();
+    }
+
+    public void showBag(){
+        BagButton.interactable = false; 
+        AttackButton.interactable = false; 
+        BagPanel.SetActive(true);
     }
 
 }
